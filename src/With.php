4d039028cc28130da06b5da635553e6663945450
nix <nix@>
2025-09-27 15:05:28 +0200

    fixed With::new() and updated / corrected PHPdocs

--- src/With.php
+++ src/With.php
@@ -1,19 +1,23 @@
 <?php
 namespace nixn\php;
 
-/** @since 1.1 */
+/**
+ * @template T
+ * @type With<T>
+ * @since 1.1
+ */
 final class With
 {
 	/**
-	 * @template T
-	 * @param class-string<T> $class
-	 * @param mixed ...$args
-	 * @return T
+	 * @template W
+	 * @param class-string<W> $class the class name to create
+	 * @param mixed ...$args constructor arguments
+	 * @return With<W> the created object, wrapped into `With`
 	 * @since 1.1
 	 */
-	public static function new(string $class, mixed ...$args): object
+	public static function new(string $class, mixed ...$args): With
 	{
-		return new $class(...$args);
+		return new self(new $class(...$args));
 	}
 
 	public function __construct(
